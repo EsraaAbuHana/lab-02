9f3b2d63245580d132fb1ea255ababb50a7a9a5f
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-02-16 23:23:18 +0200

    --

--- js/app.js
+++ js/app.js
@@ -1,54 +1,109 @@
-'use-strict';
-let keywordArr=[];
-function Image(value) {
-    this.image_url=value.image_url;
-    this.title=value.title;
-    this.description=value.description;
-    this.keyword=value.keyword;
-    this.horns=value.horns;
-    
+'use strict';
+// let templateId='#template-img';
+let keywordArr = [];
+let objArr = [];
+console.log(keywordArr);
+// creat constructor
+function Image(val) {
+    this.image_url = val.image_url;
+    this.title = val.title;
+    this.description = val.description;
+    this.keyword = val.keyword;
+    this.horns = val.horns;
+    objArr.push(this);
 }
-$.ajax('./data/page-1.json')
-.then(data=>{
-    data.forEach((item)=>{
-        console.log(item);
-        let newImage = new Image(item);
-        console.log(newImage);
-        newImage.render();
-        if (!(keywordArr.includes(newImage.keyword))) {
-         keywordArr.push(newImage.keyword);  
-         
-         let optionClone=$('.newOption').clone();
-         optionClone.removeClass('newOption');
-         optionClone.text(newImage.keyword);
-         optionClone.attr('value',`${newImage.keyword}`);
-         $('select').append(optionClone);
+console.log(objArr);
+var page;
+$('.page1').on('click', () => {
+    $('.container').empty();
+    $('.newOption').remove();
+    card('./page-1.json')
+})
+$('.page2').on('click', () => {
+    $('.container').empty();
+    $('.newOption').remove();
+    card('./page-2.json')
+})
+
+function card(page) {
+
+    $.ajax(`${page}`)
+        .then(data => {
+            data.forEach(element => {
+                let newImage = new Image(element);
+                newImage.render();
+                if (!(keywordArr.includes(newImage.keyword))) {
+                    keywordArr.push(newImage.keyword);
+                    addOption(newImage);
+                }
+            });
+            ;
         }
+
+        )
+    // to html function
+    Image.prototype.toHtml = function () {
+        let template = $('#template-img').html();
+        let newCard = Mustache.render(template, this);
+        return newCard;
+    }
+
+    Image.prototype.render = function () {
+        let divClone = this.toHtml();
+        $('.container').append(divClone);
+    };
+
+
+    // response to select
+    $('select').on('change', function () {
+        $('.container').empty();
+        // $('.container').html("");
+
+
+        let choosenImage = ($(this).val());
+        $.ajax(`${page}`)
+            .then(data => {
+                data.forEach(element => {
+                    if (element.keyword === choosenImage) {
+                        let newImage = new Image(element);
+                        newImage.render();
+                    }
+                })
+
+            })
 
     })
 
-})
-
-console.log(keywordArr);
-
-
-// $('select').on('change	',function(){
-//     console.log($(this));
-//     console.log($(this).val());
-// })
-
-
-Image.prototype.render = function(){
-    
-    let photoClone = $('.photo-template').clone();
-    photoClone.removeClass('photo-template');
-    photoClone.find('img').attr("src",`${this.image_url}`);
-
-    photoClone.find('h2').text(this.title);
-    photoClone.find('p').text(this.description);
-
-    $('.render').append(photoClone);
+    function addOption(newImage) {
+        let optionClone = $('.optionImg').clone();
+        optionClone.removeClass('optionImg');
+        optionClone.text(newImage.keyword);
+        optionClone.attr('value', `${newImage.keyword}`)
+        $('#selectImg').append(optionClone);
+    };
 }
+// sort
 
+// if ($('#sortImg'.value=='title')){
 
+    $('#sortImg').on('change', function () {
+    objArr.sort((a,b) => {
 
+       if (a.title.toUpperCase() < b.title.toUpperCase()){
+        //  console.log(a.name.toUpperCase())
+         return 1;
+       }
+        else if (a.title.toUpperCase() > b.title.toUpperCase()) return -1;
+        else return 0;
+
+    });
+    })
+    // else  {objArr.sort((a,b) => {
+
+    //     if (a.horns.toUpperCase() < b.horns.toUpperCase()){
+    //      //  console.log(a.name.toUpperCase())
+    //       return 1;
+    //     }
+    //      else if (a.horns.toUpperCase() > b.horns.toUpperCase()) return -1;
+    //      else return 0;
+    //  });}
